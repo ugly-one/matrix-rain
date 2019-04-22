add7b9d31dd633c8c94c049cfd96d721d7e3e746
Tomasz <tomasz@gnatonski.com>
2019-04-22 20:23:50 +0200

    logging also to console

---
+++ matrixGif/DebugHelpers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace matrixGif
+{
+    public static class DebugHelpers
+    {
+        public static (T result, float timeElapsed) MeasureTime<T>(Func<T> argFunc){
+            var stopwatch = Stopwatch.StartNew();
+            var result = argFunc();
+            var time = stopwatch.ElapsedMilliseconds;
+            return (result, time);
+        }
+    }
+}
--- matrixGif/Log.cs
+++ matrixGif/Log.cs
@@ -18,11 +18,13 @@ namespace matrixGif
             file = new System.IO.StreamWriter($"{Environment.CurrentDirectory}/{logName}", true);
             file.WriteLine(message);
             file.Dispose();
+            Console.WriteLine(message);
         }
         internal void Write(string[] message) {
             file = new System.IO.StreamWriter($"{Environment.CurrentDirectory}/{logName}", true);
             file.WriteLine(message.Aggregate((s1, s2) => $"{s1}, {s2}"));
             file.Dispose();
+            Console.WriteLine(message);
         } 
     }
 }
